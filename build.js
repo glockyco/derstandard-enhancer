4cf49f3256764972ad6c9f6efeba23a762a30f16
Johann Glock <11704293+glockyco@users.noreply.github.com>
2026-08-08 14:42:33 +0200

    Move launcher away from page controls

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const header = `// ==UserScript==
 // @name         DerStandard Enhancer
 // @namespace    https://www.derstandard.at/
-// @version      1.9.0
+// @version      2.0.0
 // @description  Entdeckung, Lesefortschritt und Kommentare für derStandard
 // @match        https://www.derstandard.at/*
 // @match        https://derstandard.at/*
