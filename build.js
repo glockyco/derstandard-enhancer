3249ff5e5f87eccad00f8b31964e6946a0da807d
Johann Glock <11704293+glockyco@users.noreply.github.com>
2026-08-08 14:38:49 +0200

    Nest article outline subsections

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const header = `// ==UserScript==
 // @name         DerStandard Enhancer
 // @namespace    https://www.derstandard.at/
-// @version      1.6.0
+// @version      1.7.0
 // @description  Entdeckung, Lesefortschritt und Kommentare für derStandard
 // @match        https://www.derstandard.at/*
 // @match        https://derstandard.at/*
